a74ad8703dbc73da2368601a9cd8c94b5a384e12
palhamel <pal.hamel@gmail.com>
2020-04-23 16:58:26 +0200

    Completed tutorial with lots of cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import { Card } from 'lib/Card'
+import React from "react";
+import { Card } from "lib/Card";
+
+import styled from "styled-components/macro";
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`;
 
 export const App = () => {
   return (
     <div>
-      <Card title="This is the Title text via props" secondaryText="Secondary Text via props"/>
+      <ThinnerCard
+        coverImage="https://placebear.com//500/300"
+        thumbnailUrl="http://placebeard.it/100/100"
+        title="Card title here"
+        secondaryText="Secondary Text here"
+      />
+
+      <ThinnerCard
+        coverImage="https://placebear.com//500/300"
+        thumbnailUrl="http://placebeard.it/100/100"
+        title="Card title here"
+        secondaryText="Secondary Text here"
+      >
+        Child content here
+      </ThinnerCard>
+      <Card title="Card title here" secondaryText="Secondary Text here" />
+
+      <Card
+        coverImage="https://placebear.com//500/300"
+        thumbnailUrl="http://placebeard.it/100/100"
+        title="Card title here"
+        secondaryText="Secondary Text here"
+      />
+      <Card>
+        <h1>hej hej</h1>
+      </Card>
     </div>
-  )
-}
+  );
+};
--- src/lib/Card.js
+++ src/lib/Card.js
@@ -1,33 +1,69 @@
-import React from 'react'
+import React from "react";
 
-import styled from 'styled-components/macro'
-
+import styled from "styled-components/macro";
 
 const Container = styled.div`
-  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0,0,0,.12);
-  padding: 20px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  /* padding: 20px; */
   border-radius: 6px;
   background: white;
-
-
-`
+  margin-bottom: 20px;
+`;
 
 const Title = styled.h1`
   margin: 0px;
   font-size: 24px;
-
-`
+`;
 
 const SecondaryText = styled.p`
   margin: 0px;
-  color:  grey;
+  color: grey;
+`;
 
+const Thumbnail = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background-image: url(${(props) => props.url});
+  background-size: cover;
+  margin-right: 10px;
+`;
+
+const TitleBar = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CoverImage = styled.img`
+  width: 100%;
+  border-radius: 6px 6px 0 0;
+`;
+
+const Content = styled.div`
+  padding: 20px;
+`;
+
+const ChildrenContent = styled.div`
+  background: lightsalmon;
+  padding: 10px;
+  border-radius: 6px;
 `
 
-
-export const Card = ({ title, secondaryText}) => (
-  <Container>
-    {title && <Title>{ title }</Title>}
-    {secondaryText && <SecondaryText>{ secondaryText }</SecondaryText>}
+export const Card = ({ title, secondaryText, thumbnailUrl, coverImage, className, children }) => (
+  <Container className={className}>
+    {coverImage && <CoverImage src={coverImage} />}
+    <Content>
+      <TitleBar>
+        {thumbnailUrl && <Thumbnail url={thumbnailUrl} />}
+        <div>
+          {title && <Title>{title}</Title>}
+          {secondaryText && <SecondaryText>{secondaryText}</SecondaryText>}
+        </div>
+      </TitleBar>
+      {children && <ChildrenContent>{children}</ChildrenContent>}
+    </Content>
   </Container>
-)
+);
+
+// {thumbnailUrl && .. - teknik för att ladda comp enbart om props är närvarande. Man slipper tomma objekt.
